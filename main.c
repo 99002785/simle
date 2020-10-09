eb28e6455abf908df920ba60dbc78dccc9445f49
99002785 <72495720+99002785@users.noreply.github.com>
2020-10-09 09:50:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
-#include<function.h>
+#include <stdio.h>
+
+#include<math.h>
+int add(int input1,int input2);
+int subtract(int input1,int input2);
+int multiply(int input1,int input2);
+int divide(int input1,int input2);
+double squareroot(double triginput);
+int modulus(int input1,int input2);
+int fact(int input1);
+double cosine(double triginput);
+double sine(double triginput);
+int power(int input1,int input2);
 
 int main()
 {
